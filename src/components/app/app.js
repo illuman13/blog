cd8c00f7e4fdc4cc597b58518e1dc3bead98b9c5
illuman13 <103620412+illuman13@users.noreply.github.com>
2022-11-01 19:43:47 +0700

    Update app.js

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CreateArticle from '../createArticle';
